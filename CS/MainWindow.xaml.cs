87e608f5b2cf7b5834c500eb5fb2395b46c75031
goncharov.vasily <vasily.goncharov@devexpress.com>
2020-03-31 17:54:45 +0300

    add example content

---
+++ CS/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿using DevExpress.Xpf.Core;
+
+namespace SplashScreenService
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : ThemedWindow {
+        public MainWindow() {
+            InitializeComponent();
+        }
+    }
+}
