00504d48dc75fdbfc8649a964609c8b81b9b0ed6
DevExpressExampleBot <fakeEmail>
2020-04-30 16:18:52 +0300

    Source auto update [skip ci]

---
+++ VB/View/ThemedSplashScreen/ThemedSplashScreen.xaml.vb
@@ -0,0 +1,11 @@
+﻿Imports DevExpress.Xpf.Core
+
+Namespace SplashScreenService.View
+	Partial Public Class ThemedSplashScreen
+		Inherits SplashScreenWindow
+
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+	End Class
+End Namespace
---
+++ VB/ViewModel/MainViewModel.vb
@@ -0,0 +1,31 @@
+﻿Imports DevExpress.Mvvm
+Imports DevExpress.Mvvm.DataAnnotations
+Imports DevExpress.Mvvm.POCO
+Imports System
+Imports System.Threading
+
+Namespace SplashScreenService.ViewModel
+	<POCOViewModel>
+	Public Class MainViewModel
+		Public Overridable Property Delay() As Integer
+
+		Protected ReadOnly Property SplashScreenManagerService() As ISplashScreenManagerService
+			Get
+				Return Me.GetService(Of ISplashScreenManagerService)()
+			End Get
+		End Property
+
+		Public Sub New()
+			Delay = 5
+		End Sub
+
+		Public Sub Display()
+			SplashScreenManagerService.ViewModel = New DXSplashScreenViewModel()
+			SplashScreenManagerService.ViewModel.Subtitle = "Powered by DevExpress"
+			SplashScreenManagerService.ViewModel.Logo = New Uri("../../Images/Logo.png", UriKind.Relative)
+			SplashScreenManagerService.Show()
+			Thread.Sleep(TimeSpan.FromSeconds(Delay))
+			SplashScreenManagerService.Close()
+		End Sub
+	End Class
+End Namespace
